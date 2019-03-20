9910060bd33b66f205c757fb069cb55881de152c
Vadim <vadimgb@yandex.ru>
2019-03-20 11:49:54 +0700

    noweb

---
+++ prob1/ddist.py
@@ -0,0 +1,10 @@
+class DDist:
+        def __init__(self, dictionary):
+                self.d = dictionary
+        def draw(self):
+                x = random()
+                help = 0
+                for k in self.d:
+                        help += self.d[k]
+                        if help > x:
+                                return k
