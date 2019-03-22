7d6fc0f730ae2843ae21cf5e1adfe391c65cfccd
Vadim <vadimgb@yandex.ru>
2019-03-22 14:04:20 +0700

    условная вероятность

--- prob1/ddist.py
+++ prob1/ddist.py
@@ -1,13 +1,26 @@
+from random import random
 class DDist:
         def __init__(self, dictionary):
+                '''dictionary - словарь в нем 
+                ключи - собыбтия, значения их вероятности'''
                 self.d = dictionary
+        def prob(self, k):
+                '''вычисляет вероятность события k'''
+                if k in self.d:
+                        return self.d[k]
+                else:
+                        return 0
         def draw(self):
+                '''генерирует псевдослучайное собыите'''
                 x = random()
                 help = 0
                 for k in self.d:
                         help += self.d[k]
                         if help > x:
                                 return k
-</code>
-</pre>
-
+        def trials(self, nTrials):
+                '''вычисляет список из nTrials псевдослучайных событий'''
+                return [self.draw() for k in range(nTrials)]
+#H орел, T - решка.
+dm = DDist({"H":1/2, "T":1/2})
+print(dm.trials(8))
