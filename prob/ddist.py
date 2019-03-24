eecf76319860a07b8632f0c4d1191ed157b1f430
Vadim <vadimgb@yandex.ru>
2019-03-25 01:44:11 +0700

    back to html

--- prob/ddist.py
+++ prob/ddist.py
@@ -21,13 +21,3 @@ class DDist:
         def trials(self, nTrials):
                 '''вычисляет список из nTrials псевдослучайных событий'''
                 return [self.draw() for k in range(nTrials)]
-#H орел, T - решка.
-dm = DDist({"H":1/2, "T":1/2})
-print(dm.trials(8))
-def pTestGivenDis(d):
-        if d == "болен":
-                return ddist({"положительный":0.99, "отрицательный":0.01})
-        elif d == "здоров":
-                return ddist({"положительный":0.001, "отрицательный":0.999})
-        else:
-                rise Exception("неправильное значение для d")
