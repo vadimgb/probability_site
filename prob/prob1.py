ad3d95dfb032a2b58700d132e44d9a22febc0313
Vadim <vadimgb@yandex.ru>
2019-04-22 20:25:10 +0700

    ups

--- prob/prob1.py
+++ prob/prob1.py
@@ -50,16 +50,16 @@ def descartes(a, b):
         else:
                 return (a, b)
 
-PA = DDist({"a1":0.9, "a2":0.1})
+PA = DDist({'a1':0.9, 'a2':0.1})
 def PBgA(a):
-        if a == "a1":
-                return DDist({"b1":0.7, "b2":0.3})
+        if a == 'a1':
+                return DDist({'b1':0.7, 'b2':0.3})
         else:
-                return DDist({"b1":0.2, "b2":0.8})
+                return DDist({'b1':0.2, 'b2':0.8})
 
 PAB = JDist(PA, PBgA)
 print(PAB)
-PAB = DDist({("H", "H"):1/4, ("H", "T"):1/4, ("T", "H"):1/4, ("T", "T"):1/4})
+PAB = DDist({('H', 'H'):1/4, ('H', 'T'):1/4, ('T', 'H'):1/4, ('T', 'T'):1/4})
 PA = PAB.marginalizeOut(1)
 print(PA)
 def makeProbField(name, omega):
@@ -98,10 +98,10 @@ def makeProbField(name, omega):
 from fractions import Fraction
 p = Fraction('1/6')
 pCube = DDist({1:p, 2:p, 3:p, 4:p, 5:p, 6:p})
-Cube = makeProbField("Cube", pCube)
+Cube = makeProbField('Cube', pCube)
 a = Cube(lambda x: x % 2 == 0)
 b = Cube(lambda x: x > 3)
 d = a + b
-print(f"{a} prob={a.prob}")
-print(f"{b} prob= {b.prob}")
-print(f"{d} prob = {d.prob}")
+print(f'{a} prob={a.prob}')
+print(f'{b} prob= {b.prob}')
+print(f'{d} prob = {d.prob}')
